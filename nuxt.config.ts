dce8616721eeaba8a397bcf4bdfdf6b91869bf9a
akihisa.makimoto <akihisa2359@gmail.com>
2024-06-22 19:10:27 +0900

    tailwind無しの方が良さそうなので利用しない方向で修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css?family=Pacifico",
+          crossorigin: "",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Montserrat&display=swap",
+          crossorigin: "",
+        },
+      ],
+    },
+  },
   devtools: { enabled: true },
   css: ["~/assets/destyle.css", "~/assets/main.scss"],
+  // modules: ["@nuxtjs/tailwindcss"],
   vite: {
     css: {
       preprocessorOptions: {
